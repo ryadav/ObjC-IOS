a02101d3d676946fd4bb18aee39ea9fe978cf55f
Rahul Yadav <rahulyadav44it@gmail.com>
2017-11-22 17:52:16 +0530

    After success tag added

--- scripts/testflight.sh
+++ scripts/testflight.sh
@@ -14,13 +14,17 @@ RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
 zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 
+mkdir -p "$OUTPUTDIR/Payload"
+cp -R "$OUTPUTDIR/$APP_NAME.app" Payload/
+zip -r -s 64 Payload.zip Payload/
+mv Payload.zip "$APP_NAME.ipa"
   
 curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
   -F status="2" \
   -F notify="0" \
   -F notes="$RELEASE_NOTES" \
   -F notes_type="0" \
-  -F ipa="@$OUTPUTDIR/$APP_NAME.app" \
+  -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
   -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
   -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
   
