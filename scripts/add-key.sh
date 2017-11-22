50a7c13dbe7ebb9b71cd171f572f8c5f9586cee3
Rahul Yadav <rahulyadav44it@gmail.com>
2017-11-22 13:06:28 +0530

    Added provisioning profile

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -39,5 +39,6 @@ security set-keychain-settings -t 3600 -u $KEY_CHAIN
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/TCSL.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/TCSL.p12 -k ~/Library/Keychains/ios-build.keychain -P tcs@123 -T /usr/bin/codesign
+cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
