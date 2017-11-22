861044412fe5811fe0d4ef0dfa01b197d0e3f7d1
Rahul Yadav <rahulyadav44it@gmail.com>
2017-11-22 15:40:17 +0530

    Update hockey app url

--- scripts/testflight.sh
+++ scripts/testflight.sh
@@ -15,10 +15,11 @@ RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 zip -r -9 "$OUTPUTDIR/$APPNAME.app.dSYM.zip" "$OUTPUTDIR/$APPNAME.app.dSYM"
 
   
-curl http://testflightapp.com/api/builds.json \
- -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
- -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
- -F api_token="$TESTFLIGHT_API_TOKEN" \
- -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
- -F distribution_lists='Internal' \
- -F notes="$RELEASE_NOTES"
+curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
+  -F status="2" \
+  -F notify="0" \
+  -F notes="$RELEASE_NOTES" \
+  -F notes_type="0" \
+  -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+  -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
