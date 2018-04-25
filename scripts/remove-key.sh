2750094bbb3765c63a371f97fdf86fafffad1c66
rahulyadav <rahulyadav44it@gmail.com>
2018-04-26 00:19:22 +0530

    kl

--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -4,7 +4,7 @@ rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovisio
 
 
 jsonValue= cat ./scripts/customConfiguration.json | jq '.isCreateBuild'
-if [[ "$jsonValue" == "false" ]]; then
+if [ "$jsonValue" = true ]; then
     echo "This is a pull request. No deployment will be done."
 fi
 
