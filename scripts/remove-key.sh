fc3185bc808fa18399988ff727757e471d178756
Rahul Yadav <rahulyadav44it@gmail.com>
2018-04-25 19:49:40 +0530

    fine renamed

--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -3,6 +3,6 @@ security delete-keychain ios-build.keychain
 rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
 
 
-jsonValue=`cat loaded.json | json select '.isCreateBuild'`
+jsonValue=`cat customConfiguration.json | json select '.isCreateBuild'`
 echo $jsonValue
 echo "my name appear here rahul yadav"
